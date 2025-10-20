c88b4cc4a223d3924716ac8cf5dfd91ccf799aef
Miguel Lumapat <lamppot@protonmail.com>
2025-10-19 23:11:11 -0400

    Add simple arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+/// Processes an input CSV file of payments transactions
+/// and outputs a CSV file of outstanding account balances
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path of the input CSV file
+    input_file: PathBuf,
+
+    /// Emit debug
+    #[arg(short, long, default_value_t = false)]
+    debug: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Hello, world! {:#?}", args);
 }
