911b1390f613cc7abf2ee0c2748f1e68607b44fe
Miguel Lumapat <lamppot@protonmail.com>
2025-10-20 02:45:38 -0400

    Make Amount negatable

--- src/toy_payments/amount.rs
+++ src/toy_payments/amount.rs
@@ -1,24 +1,24 @@
-use std::ops::{Add, AddAssign, Sub, SubAssign};
+use std::ops::{Add, AddAssign, Neg, Sub, SubAssign};
 
 // A custom Amount type since we're doing financial transactions
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Amount(u64);
+pub struct Amount(i64);
 
 impl Amount {
     pub fn new(whole_units: u64) -> Self {
-        Self(whole_units * 10000)
+        Self((whole_units * 10000) as i64)
     }
 }
 
 impl From<u64> for Amount {
     fn from(value: u64) -> Self {
-        Self(value * 10000)
+        Self((value * 10000) as i64)
     }
 }
 
 impl From<f64> for Amount {
     fn from(value: f64) -> Self {
-        Self((value * 10000.0) as u64)
+        Self((value * 10000.0) as i64)
     }
 }
 
@@ -51,5 +51,12 @@ impl Sub for Amount {
 impl SubAssign for Amount {
     fn sub_assign(&mut self, other: Self) {
         self.0 -= other.0;
+    }
+}
+
+impl Neg for Amount {
+    type Output = Self;
+    fn neg(self) -> Self {
+        Self(-self.0)
     }
 }
